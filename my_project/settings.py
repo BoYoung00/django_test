0c6f17ccb23f067d67dc2dc97d29a1220c3b4014
BoYoung00 <qhdud20@naver.com>
2023-07-20 20:53:26 +0900

    23/07/20 장고를 이용한 백엔드 연습

--- my_project/settings.py
+++ my_project/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ # 앱 등록
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mysite',
     'pages',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_IRL = '/'
--- mysite/views.py
+++ mysite/views.py
@@ -1,13 +1,20 @@
 # from django.http import HttpResponse
-from  django.shortcuts import render
+from  django.shortcuts import get_object_or_404, render
 from .models import MainContent
 
 def index(requset):
     # return HttpResponse("Hello world")
 
     # 가장 최신 컨텐츠를 상단에 노출
-    content_list = MainContent.objects.order_by('-pub_date')
+    content_list = MainContent.objects.order_by('-pub_date') # order_by('-pub_date')는 정렬
     context = {'content_list':content_list}
     # render() 함수는 content_list의 데이터를 mysite/content_list.html 파일에 적용 후 html을 리턴
     return render(requset, 'mysite/content_list.html', context)
     # mysite / content_list.html는 템플릿임
+
+def detail(request, content_id):
+    # http://localhost:8000/mysite/2/
+    # content_list = MainContent.objects.get(id=content_id) # content_id는 url의 2
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = { 'content_list': content_list}
+    return  render(request, 'mysite/content_detail.html', context)
